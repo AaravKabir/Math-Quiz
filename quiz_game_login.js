f1bed322662d41f4c0914d462d3762ec68a7af6b
AaravKabir <75346593+AaravKabir@users.noreply.github.com>
2021-08-07 13:05:44 -0400

    Delete quiz_game_login.js

--- quiz_game_login.js
+++
@@ -1,8 +0,0 @@
-function add_user(){
-    var player1_name = document.getElementById("player1name_input").value;
-    var player2_name = document.getElementById("player2name_input").value;
-
-    localStorage.setItem("player1_name", player1_name);
-    localStorage.setItem("player2_name", player2_name);
-    window.location="quiz_game_page.html";
-}
